17a6c2a096df11691984c6788ede76fb6ef2de6e
Andres Rossi <andresrossi2022@gmail.com>
2023-04-18 12:26:49 -0300

    funcionalidad extra a pagina

--- PreEntrega3_Andres-Rossi/calcularImc.js
+++ PreEntrega3_Andres-Rossi/calcularImc.js
@@ -1,12 +1,22 @@
 class IMCCalculator {
-  constructor(peso, altura) {
+  constructor(peso, altura, sexo, edad) {
     this.peso = peso;
     this.altura = altura;
+    this.sexo = sexo;
+    this.edad = edad;
   }
   calcularImc() {
-    let indiceEntero = this.peso / (this.altura * this.altura);
-    return indiceEntero.toFixed(2);
+    let indice;
+    if (this.sexo === 'Hombre') {
+      indice = this.peso / (this.altura * this.altura) - 0.0022 * this.edad;
+    } else {
+      indice =
+        (this.peso * 0.9) / (this.altura * this.altura) -
+        7.9 * (this.edad / 100);
+    }
+    return indice.toFixed(2);
   }
+
   interpretarIMC(imc) {
     const rangos = [
       { valor: 16, mensaje: 'Delgadez extrema' },
@@ -32,10 +42,14 @@ const calcularBtn = document.querySelector('#calcular-btn');
 const resultadoDiv = document.querySelector('#resultado');
 
 calcularBtn.addEventListener('click', () => {
+
   const peso = parseFloat(document.querySelector('#peso').value);
   const altura = parseFloat(document.querySelector('#altura').value) * 0.01;
+  const sexo = document.querySelector('#sexo').value;
 
-  const imcCalculator = new IMCCalculator(peso, altura);
+  const edad = parseInt(document.querySelector('#edad').value);
+  const imcCalculator = new IMCCalculator(peso, altura, sexo, edad);
+
   const imc = imcCalculator.calcularImc();
   const interpretacionIMC = imcCalculator.interpretarIMC(imc);
   //Guardar el local Storage
@@ -65,53 +79,52 @@ function mostrarConsejos(imc) {
   document.querySelector('#consejos').innerHTML = consejos;
 }
 
-/*let consejos = '';
-  switch (true) {
-    case imc < 16:
-      consejos =
-        'Tu IMC indica que tienes delgadez severa. Es posible que estés por debajo de tu peso saludable. Para aumentar de peso, es importante que comas alimentos ricos en nutrientes y calorías, como frutos secos, semillas, aguacate, plátanos, carne, pescado, etc.';
-      break;
-    case imc >= 16 && imc < 18.5:
-      consejos =
-        'Tu IMC indica que tienes un peso inferior al normal. Es importante que comas una dieta equilibrada y saludable para aumentar de peso de manera saludable. Incluye alimentos ricos en nutrientes como frutas, verduras, carnes magras, pescado, frutos secos, semillas, etc.';
-      break;
-    case imc >= 18.5 && imc < 25:
-      consejos =
-        'Tu IMC indica que tienes un peso saludable. Sigue comiendo una dieta equilibrada y saludable y mantente activo para mantener tu peso saludable.';
-      break;
-    case imc >= 25 && imc < 30:
-      consejos =
-        'Tu IMC indica que tienes sobrepeso. Es importante que comiences a hacer cambios en tu dieta y actividad física para perder peso y mejorar tu salud. Comienza por comer menos alimentos procesados y más frutas, verduras, granos enteros, proteínas magras y grasas saludables. También puedes aumentar tu actividad física diaria, como caminar, nadar, correr, etc.';
-      break;
-    case imc >= 30:
-      consejos =
-        'Tu IMC indica que tienes obesidad. Es importante que hagas cambios significativos en tu dieta y actividad física para perder peso y mejorar tu salud. Habla con tu médico sobre las opciones de tratamiento para la obesidad y comienza por comer menos alimentos procesados y más frutas, verduras, granos enteros, proteínas magras y grasas saludables. También puedes aumentar tu actividad física diaria, como caminar, nadar, correr, etc.';
-      break;
-    default:
-      consejos = 'Ha ocurrido un error al mostrar los consejos.';
-      break;
-  }
-  document.querySelector('#consejos').innerHTML = consejos;
-}*/
-
-function calcularIMC() {
+/*function calcularIMC() {
   const peso = parseFloat(document.querySelector('#peso').value);
   const altura = parseFloat(document.querySelector('#altura').value);
+  const sexo = document.querySelector('input[name="sexo"]:checked').value;
+  const usuario = new Usuario(sexo, peso, altura);
 
-  const usuario = new Usuario('', 0, peso, altura);
-  const imc = usuario.calcularImc();
-  const interpretacionIMC = usuario.interpretarIMC(imc);
+  const imcCalculator = new IMCCalculator(peso, altura, sexo, edad);
+  const imc = imcCalculator.calcularImc();
+  const interpretacionIMC = imcCalculator.interpretarIMC(imc);
+
 
   const resultadoDiv = document.querySelector('#resultado');
   resultadoDiv.innerHTML = `<p>Su IMC es ${imc} e indica que tiene "${interpretacionIMC}"</p>`;
+}*/
+function calcularIMC() {
+  const peso = usuario.getPeso();
+  const altura = usuario.getAltura();
+  const sexo = usuario.getSexo();
+  const edad = usuario.getEdad();
+
+  const imcCalculator = new IMCCalculator(
+    peso,
+    altura,
+    sexo,
+    usuario.getEdad()
+  );
+  const imc = imcCalculator.calcularImc();
+  const interpretacionIMC = imcCalculator.interpretarIMC(imc);
+
+  resultadoDiv.innerHTML = `<p>Su IMC es ${imc} e indica que tiene "${interpretacionIMC}"</p>`;
+  mostrarConsejos(imc);
+
+  //Guardar el local Storage
+  localStorage.setItem('peso', peso);
+  localStorage.setItem('altura', altura);
+  localStorage.setItem('edad', edad);
+  localStorage.setItem('sexo', sexo);
 }
 
 // Objeto que representa un usuario
 class Usuario {
-  constructor(nombre, edad, peso, altura) {
+  constructor(nombre, edad, peso, altura, sexo) {
     this.nombre = nombre;
     this.edad = edad;
     this.peso = peso;
+    this.sexo = sexo;
     this.altura = altura;
   }
 }
